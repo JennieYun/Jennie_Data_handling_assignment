354e501a73837c8955bf2fecd9c44a19accee6b4
JennieYun <jennie.yun@slu.se>
2025-01-28 14:51:24 +0100

    pivot_longer()

--- Data_cleaning.R
+++ Data_cleaning.R
@@ -10,6 +10,8 @@ sulfur <- read_csv("tidytuesday:2020:2020-05-12:volcano/sulfur.csv")
 tree_ring <- read_csv("tidytuesday:2020:2020-05-12:volcano/tree_rings.csv")
 volcanos <- read_csv("tidytuesday:2020:2020-05-12:volcano/volcano.csv")
 
+#1900-2020년 사이에 n_tree 혹은 europe index가 음수였던 데이터를 필터링한 후 그 연도에 다른 연도보다 더 많은 화산활동이 많았다는 것을 보여줘도 좋을 듯
+
 View(eruptions) # volcano name & vei & coordinates
 View(events) # volcano name & volcano type -> explosion type might effect weather/temp during that time
 View(sulfur) # link sulfur and eruptions
@@ -32,5 +34,13 @@ sulfur_tree_ring <- sulfur_year %>%
 
 View(sulfur_tree_ring)
 
+
+# 2.  tree_ring 데이터를 pivot_longer()를 사용하여 n_tree와 europe_temp_index를 하나의 measurement 변수로 합쳐서 하나의 긴 데이터로 변환할 수 있습니다.
+
+measurement <- sulfur_tree_ring %>%
+pivot_longer(cols = c(n_tree, europe_temp_index, mean_neem, mean_wdc), 
+             names_to = "measurement", values_to = "value")
+
+View(measurement)
 
 
